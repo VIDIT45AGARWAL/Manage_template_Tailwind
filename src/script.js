952983872a0c9db3ac48435619e96b5105b4bad7
Vidit Agarwal <viditagarwal0123@gmail.com>
2025-05-15 12:00:44 +0530

    hamburger_menu done

--- src/script.js
+++ src/script.js
@@ -1,7 +1,11 @@
 const butn =document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
+const hamburger = document.getElementById('ham');
+const cross = document.getElementById('cross');
 
 butn.addEventListener('click', ()=>{
     menu.classList.toggle('flex');
     menu.classList.toggle('hidden');
+    hamburger.classList.toggle('hidden');
+    cross.classList.toggle('hidden');
 })
